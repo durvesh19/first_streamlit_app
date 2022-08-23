9068bdc864ded49c9f180659353d3cdbee14b378
Durvesh_Thakur <111852836+durvesh19@users.noreply.github.com>
2022-08-23 14:33:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.subtitle('Breakfast Menu Omega 3 & Blueberry Oatmeal Kale, Spinach & Rocket Smoothie Hard-Boiled Free-Range Egg')
